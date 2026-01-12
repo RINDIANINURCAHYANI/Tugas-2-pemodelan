d9f938f2783f1f08a83b6b64ca206a55812fc467
RINDIANINURCAHYANI <rindianinurcahyani5@gmail.com>
2026-01-11 19:22:23 -0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
 from flask import Flask, render_template, request
+import matplotlib
+matplotlib.use("Agg")  # WAJIB untuk server (Railway)
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
@@ -29,7 +32,7 @@ def index():
                 'Wq': round(Wq, 4)
             }
 
-            # Grafik sederhana
+            # Grafik
             labels = ['Waktu Antrian (Wq)', 'Waktu Sistem (W)']
             values = [Wq, W]
 
@@ -37,10 +40,12 @@ def index():
             plt.bar(labels, values)
             plt.title("Perbandingan Waktu Antrian dan Waktu Sistem")
             plt.ylabel("Waktu (menit)")
+            plt.tight_layout()
             plt.savefig("static/grafik.png")
             plt.close()
 
-        except:
+        except Exception as e:
+            print("ERROR:", e)
             hasil = "error"
 
     return render_template('index.html', hasil=hasil)
