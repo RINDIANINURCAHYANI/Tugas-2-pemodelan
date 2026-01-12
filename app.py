f73043fd14e058b2bfb462879d526bc7e06eb8e3
RINDIANINURCAHYANI <rindianinurcahyani5@gmail.com>
2026-01-12 11:09:53 -0900

    tugas 2 ps

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    hasil = None
+
+    if request.method == 'POST':
+        try:
+            interarrival = float(request.form['interarrival'])
+            service_time = float(request.form['service'])
+
+            if interarrival <= 0 or service_time <= 0:
+                raise ValueError
+
+            lamda = 1 / interarrival
+            mu = 1 / service_time
+            rho = lamda / (2 * mu)
+            W = 1 / (mu - lamda / 2)
+            Wq = (lamda ** 2) / (2 * mu * (mu - lamda / 2))
+
+            hasil = {
+                'lambda': round(lamda, 4),
+                'mu': round(mu, 4),
+                'rho': round(rho, 4),
+                'W': round(W, 4),
+                'Wq': round(Wq, 4)
+            }
+
+            # Grafik sederhana
+            labels = ['Waktu Antrian (Wq)', 'Waktu Sistem (W)']
+            values = [Wq, W]
+
+            plt.figure()
+            plt.bar(labels, values)
+            plt.title("Perbandingan Waktu Antrian dan Waktu Sistem")
+            plt.ylabel("Waktu (menit)")
+            plt.savefig("static/grafik.png")
+            plt.close()
+
+        except:
+            hasil = "error"
+
+    return render_template('index.html', hasil=hasil)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
